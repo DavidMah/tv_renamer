67a0f176a93f9bddffedd2c4e70fdcfe8acc88e4
David Mah <mahhaha@gmail.com>
2011-10-30 16:49:56 -0700

    Added Title Writer

--- lib/title_writer.rb
+++ lib/title_writer.rb
@@ -2,4 +2,13 @@ class TitleWriter
   def retrieve_current_names(directory)
     Dir.entries(directory)
   end
+  # Data is a list of pairs of filename to targetname
+  def write_names(directory = ".", data)
+    log = File.new("log.txt", "w")
+    data.each do |name_element|
+      names = name_element.map{|e| File.join(directory, e)}
+      log.syswrite "#{name_element.join(" --> ")}\n"
+      File.rename(*names)
+    end
+  end
 end
