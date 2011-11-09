25d04d1a9b7903e4097c3386f9ab3dd1fdc7301a
David Mah <mahhaha@gmail.com>
2011-11-09 00:11:45 -0800

    Added backend functionality for Title Scripting
    - describe_names lets a client script write titles that slip in Show
      title, episode title, or episode number

--- lib/title_writer.rb
+++ lib/title_writer.rb
@@ -28,4 +28,22 @@ class TitleWriter
     end
       {:status => "success"}
   end
+
+  # Create a title for every title/no using embedded symbols
+  # Embeddable symbols to get substituted:
+  # #{main_title} - Title of the TV Show
+  # #{ep_title}   - Title of the Episode
+  # #{ep_num}     - Number of the Episode
+  def describe_names(titles, script, main_title)
+
+    new_titles = Array.new(titles.size){ script }
+    new_titles.map! { |t| t.gsub(/\#{main_title}/, main_title) }
+    new_titles.map!.with_index { |t, i| t.gsub(/\#{ep_title}/, titles[i]) }
+
+    zero_pad = Math.log10(titles.size).to_i + 1
+    new_titles.map!.with_index { |t, i| t.gsub(/\#{ep_num}/, "%0#{zero_pad}d" %(i + 1)) }
+
+    new_titles
+  end
+
 end
--- spec/title_writer_spec.rb
+++ spec/title_writer_spec.rb
@@ -35,4 +35,32 @@ describe TitleWriter do
     end
   end
 
+  describe "describe_names" do
+    it "should leave these unembeddy strings alone" do
+      @title_writer.describe_names(['a', 'b', 'c'], 'garpley', 'miku').should == ['garpley', 'garpley', 'garpley']
+    end
+
+    it "should slip in the main_title if needed" do
+      @title_writer.describe_names(['a', 'b', 'c'], '#{main_title} garpley', 'miku').should == ['miku garpley', 'miku garpley', 'miku garpley']
+    end
+
+    it "should slip in the ep_title if needed" do
+      @title_writer.describe_names(['a', 'b', 'c'], '#{ep_title} garpley', 'miku').should == ['a garpley', 'b garpley', 'c garpley']
+    end
+
+    it "should slip in the ep_num if needed" do
+      @title_writer.describe_names(['a', 'b', 'c'], '#{ep_num} garpley', 'miku').should == ['1 garpley', '2 garpley', '3 garpley']
+    end
+
+    it "should drop 0s in front of there are > 9 episodes" do
+      result = @title_writer.describe_names(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'], '#{ep_num} garpley', 'miku')
+      result.should == ['01 garpley', '02 garpley', '03 garpley', '04 garpley', '05 garpley', '06 garpley', '07 garpley', '08 garpley', '09 garpley', '10 garpley']
+    end
+
+    it "should work with a combination of these substitutions" do
+      result = @title_writer.describe_names(['a', 'b', 'c'], '#{main_title} - #{ep_num} - #{ep_title}.mkv', 'Adventure Time')
+      result.should == ['Adventure Time - 1 - a.mkv', 'Adventure Time - 2 - b.mkv', 'Adventure Time - 3 - c.mkv']
+    end
+  end
+
 end
